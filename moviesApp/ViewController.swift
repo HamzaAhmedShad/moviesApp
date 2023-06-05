12586b130426cc9181addf2746e0c7bea484add6
hamzaahmedshadBS <hamza.ahmed@brainstation-23.com>
2023-06-05 15:24:46 +0600

    Initial Commit

---
+++ moviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviesApp
+//
+//  Created by BS1101 on 5/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
