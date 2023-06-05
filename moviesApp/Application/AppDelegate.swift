ec6e8cc6dae6056bd1de48897dd4fbbbbf730b8a
hamzaahmedshadBS <hamza.ahmed@brainstation-23.com>
2023-06-05 15:37:25 +0600

    MVVM Structure and Scene Delegate Deleted

---
+++ moviesApp/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  moviesApp
+//
+//  Created by BS1101 on 5/6/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
